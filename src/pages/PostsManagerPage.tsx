c2bf3ea377a821526fac53992608a837dba9a047
jungeun0712 <kje970712@naver.com>
2025-01-19 20:09:04 +0900

    refactor: 게시물 삭제 함수를 Jotai 상태 관리 방식 적용 - deletePost 함수의 id 매개변수에 number 타입 추가 - setPosts를 setPostList로 변수명 변경 - posts 필터링 로직 수정

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -226,12 +226,15 @@ const PostsManager = () => {
   }
 
   // 게시물 삭제
-  const deletePost = async (id) => {
+  const deletePost = async (id: number) => {
     try {
       await fetch(`/api/posts/${id}`, {
         method: "DELETE",
       })
-      setPosts(posts.filter((post) => post.id !== id))
+      setPostList((postItems) => ({
+        ...postItems,
+        posts: postItems.posts.filter((post) => post.id !== id),
+      }))
     } catch (error) {
       console.error("게시물 삭제 오류:", error)
     }
