fc3b82d482b72d36cd10991880d39c50499ba808
JunmanChoi <junman9595@gmail.com>
2025-01-20 01:18:00 +0900

    feat: Post엔티티 모델 추가

---
+++ src/entities/post/model/types.ts
@@ -0,0 +1,14 @@
+interface Post {
+  id: number
+  title: string
+  body: string
+  tags: string[]
+  reactions: {
+    likes: number
+    dislikes: number
+  }
+  views: number
+  userId: number
+}
+
+export type { Post }
