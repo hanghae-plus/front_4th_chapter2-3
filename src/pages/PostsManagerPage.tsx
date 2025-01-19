6cf9999d4bfb2814df75749c631e0914fff3b064
WonYoung <wy.heo@teamsparta.co>
2025-01-19 18:26:26 +0900

    ♻️ .then 형식 -> async await 으로 수정

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -70,33 +70,29 @@ const PostsManager = () => {
   }
 
   // 게시물 가져오기
-  const fetchPosts = () => {
+  const fetchPosts = async () => {
     setLoading(true)
-    let postsData: PostListRes
-    let usersData: User[]
+    try {
+      const [postsResponse, usersResponse] = await Promise.all([
+        fetch(`/api/posts?limit=${limit}&skip=${skip}`),
+        fetch('/api/users?limit=0&select=username,image'),
+      ])
 
-    fetch(`/api/posts?limit=${limit}&skip=${skip}`)
-      .then((response) => response.json())
-      .then((data) => {
-        postsData = data
-        return fetch('/api/users?limit=0&select=username,image')
-      })
-      .then((response) => response.json())
-      .then((users) => {
-        usersData = users.users
-        const postsWithUsers = postsData.posts.map((post) => ({
-          ...post,
-          author: usersData.find((user) => user.id === post.userId),
-        }))
-        setPosts(postsWithUsers)
-        setTotal(postsData.total)
-      })
-      .catch((error) => {
-        console.error('게시물 가져오기 오류:', error)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
+      const postsData: PostListRes = await postsResponse.json()
+      const usersData: UserListRes = await usersResponse.json()
+
+      const postsWithUsers = postsData.posts.map((post) => ({
+        ...post,
+        author: usersData.users.find((user) => user.id === post.userId),
+      }))
+
+      setPosts(postsWithUsers)
+      setTotal(postsData.total)
+    } catch (error) {
+      console.error('게시물 가져오기 오류:', error)
+    } finally {
+      setLoading(false)
+    }
   }
 
   // 태그 가져오기
