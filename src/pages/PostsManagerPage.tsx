7e39aed6af12b41e67d30ca3793a30c3b38b0597
WonYoung <wy.heo@teamsparta.co>
2025-01-20 22:32:46 +0900

    util 함수 분리

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -31,6 +31,7 @@ import { getPostTags } from '../legacy/service/post.service'
 import { deleteComment, getComments, patchComment, postComment, putComment } from '../legacy/service/comments.service'
 import { getUser } from '../legacy/service/user.service'
 import { usePost } from '../legacy/hooks/usePost'
+import { highlightText } from '../legacy/utils/highligtText'
 
 const PostsManager = () => {
   const navigate = useNavigate()
@@ -202,21 +203,6 @@ const PostsManager = () => {
     setSortOrder(params.get('sortOrder') || 'asc')
     setSelectedTag(params.get('tag') || '')
   }, [location.search])
-
-  // 하이라이트 함수 추가
-  const highlightText = (text: string, highlight: string) => {
-    if (!text) return null
-    if (!highlight.trim()) {
-      return <span>{text}</span>
-    }
-    const regex = new RegExp(`(${highlight})`, 'gi')
-    const parts = text.split(regex)
-    return (
-      <span>
-        {parts.map((part, i) => (regex.test(part) ? <mark key={i}>{part}</mark> : <span key={i}>{part}</span>))}
-      </span>
-    )
-  }
 
   // 게시물 테이블 렌더링
   const renderPostTable = () => (
