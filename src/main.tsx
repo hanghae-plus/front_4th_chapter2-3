b982619a19eb7f37e7f494d196c75041e20733f0
YeongseoYoon <l112xpla@gmail.com>
2025-01-20 21:56:49 +0900

    refactor: query client 위치 이동

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client"
 import { QueryClientProvider } from "@tanstack/react-query"
 import "./index.css"
 import App from "./App.tsx"
-import { queryClient } from "./shared/lib/query/index.ts"
+import { queryClient } from "./shared/api/query-client.ts"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 createRoot(document.getElementById("root")!).render(
