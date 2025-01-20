44124b28f744aa4d81468b366c3b9773818b7558
Bae seong-kyu <baesense@naver.com>
2025-01-20 23:12:55 +0900

    Refactor : Post 타입 지정

---
+++ src/features/post/types/index.ts
@@ -0,0 +1,21 @@
+export interface Post {
+  id: number
+  author: Author
+  body: string
+  reactions: Reactions
+  tags: string[]
+  title: string
+  userId: number
+  views: number
+}
+
+interface Author {
+  id: number
+  image: string
+  username: string
+}
+
+interface Reactions {
+  likes: number
+  dislikes: number
+}
