08217a60b8298761823a840f9ac2893dc0fc870f
Changyeop Song <waitwait0301@naver.com>
2025-01-20 23:14:51 +0900

    refactor: 사용자 모달에서 유저정보 entities로 분리

---
+++ src/entities/user/model/types/user.ts
@@ -0,0 +1,14 @@
+import type { Address } from "./address"
+import type { Company } from "./company"
+
+export interface User {
+  image: string
+  username: string
+  firstName: string
+  lastName: string
+  age: number
+  email: string
+  phone: number
+  address: Address
+  company: Company
+}
--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -2,6 +2,7 @@ import { Edit2, MessageSquare, Plus, Search, ThumbsDown, ThumbsUp, Trash2 } from
 import { useEffect, useState } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 
+import UserProfile from "../entities/user/ui/UserProfile"
 import { Textarea, Button, Card, Dialog, Input, Select, Table } from "../shared/ui"
 import { PostEditDialog } from "../widgets/post-dialog/ui/PostDialog"
 
@@ -671,31 +672,7 @@ const PostsManager = () => {
           <Dialog.Header>
             <Dialog.Title>사용자 정보</Dialog.Title>
           </Dialog.Header>
-          <div className="space-y-4">
-            <img src={selectedUser?.image} alt={selectedUser?.username} className="w-24 h-24 rounded-full mx-auto" />
-            <h3 className="text-xl font-semibold text-center">{selectedUser?.username}</h3>
-            <div className="space-y-2">
-              <p>
-                <strong>이름:</strong> {selectedUser?.firstName} {selectedUser?.lastName}
-              </p>
-              <p>
-                <strong>나이:</strong> {selectedUser?.age}
-              </p>
-              <p>
-                <strong>이메일:</strong> {selectedUser?.email}
-              </p>
-              <p>
-                <strong>전화번호:</strong> {selectedUser?.phone}
-              </p>
-              <p>
-                <strong>주소:</strong> {selectedUser?.address?.address}, {selectedUser?.address?.city},{" "}
-                {selectedUser?.address?.state}
-              </p>
-              <p>
-                <strong>직장:</strong> {selectedUser?.company?.name} - {selectedUser?.company?.title}
-              </p>
-            </div>
-          </div>
+          <UserProfile user={selectedUser} />
         </Dialog.Content>
       </Dialog>
     </Card>
