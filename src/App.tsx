24a058b6092dbf0a37b33aa3c8696fdc7b7ec712
Changyeop Song <waitwait0301@naver.com>
2025-01-20 20:21:11 +0900

    feat: eslint 룰 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/ui/Header.tsx"
-import Footer from "./widgets/ui/Footer.tsx"
+
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import Footer from "./widgets/ui/Footer.tsx"
+import Header from "./widgets/ui/Header.tsx"
 
 const App = () => {
   return (
