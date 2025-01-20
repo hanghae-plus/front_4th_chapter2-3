78fe26c21a7666251a571feafd4ae3c4e7ca91d7
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-01-20 16:11:23 +0900

    :truck: Move: Header, Footer shared/ui로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/ui/Header.tsx"
-import Footer from "./widgets/ui/Footer.tsx"
+
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { Header } from "./shared/ui/header"
+import { Footer } from "./shared/ui/footer"
 
 const App = () => {
   return (
