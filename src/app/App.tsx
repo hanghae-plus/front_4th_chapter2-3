0511cf64518f952a26b92c63a978b7c2a33d3bfc
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-01-21 19:00:38 +0900

    :sparkles: Feat: @tanstack/react-query provider 설정 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,21 @@
-import { RouterProvider } from "./provider"
+import { RouterProvider, ReactQueryProvider } from "./provider"
 import PostsManagerPage from "../pages/PostsManagerPage.tsx"
 import { Header } from "../shared/ui/header"
 import { Footer } from "../shared/ui/footer"
 
 const App = () => {
   return (
-    <RouterProvider>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </RouterProvider>
+    <ReactQueryProvider>
+      <RouterProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </RouterProvider>
+    </ReactQueryProvider>
   )
 }
 
--- src/app/provider/index.ts
+++ src/app/provider/index.ts
@@ -1,1 +1,2 @@
 export { RouterProvider } from "./RouterProvider"
+export { ReactQueryProvider } from "./ReactQueryProvider"
