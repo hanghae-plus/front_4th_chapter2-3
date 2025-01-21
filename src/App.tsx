0066c0609d3f3162e9c1e87a08e6a0748b779552
Changyeop Song <waitwait0301@naver.com>
2025-01-22 00:21:38 +0900

    feat: App.tsx에 QueryClientProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter as Router } from "react-router-dom"
 
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
@@ -5,16 +6,20 @@ import Footer from "./widgets/footer/ui/Footer.tsx"
 import Header from "./widgets/header/ui/Header.tsx"
 
 const App = () => {
+  const queryClient = new QueryClient()
+
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
