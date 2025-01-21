c64819d1489959cf25f7b4cbd543650164bb4e63
WonYoung <wy.heo@teamsparta.co>
2025-01-21 20:06:20 +0900

    tanstack query 초기 셋팅 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // 창이 다시 포커스될 때 refetch 여부
+      retry: 1, // 실패 시 재시도 횟수
+      staleTime: 5 * 60 * 1000, // 데이터가 오래되기 전까지의 시간(ms)
+    },
+  },
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
