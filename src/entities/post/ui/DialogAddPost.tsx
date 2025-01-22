2dfc2c01eea643d5f49483280ec46566f7dd647f
9yurilee <9yurilee@gmail.com>
2025-01-22 09:29:45 +0900

    fix: 새글추가에서 onChange 이벤트 이슈 해결

--- src/entities/post/ui/DialogAddPost.tsx
+++ src/entities/post/ui/DialogAddPost.tsx
@@ -8,6 +8,7 @@ import {
   Input,
   Button,
 } from "../../../shared/ui";
+import { Post } from "../model/types";
 
 interface DialogAddPostProps {
   onOpen: boolean;
@@ -18,15 +19,7 @@ interface DialogAddPostProps {
     body: string;
     userId: number;
   };
-  onSetNewPost: (
-    type: string,
-    postContent: {
-      id?: number;
-      title: string;
-      body: string;
-      userId: number;
-    }
-  ) => void;
+  onSetNewPost: (field: keyof Post, value: string | number) => void;
   onAddPost: () => void;
 }
 
@@ -50,28 +43,19 @@ export const DialogAddPost: React.FC<DialogAddPostProps> = ({
           <Input
             placeholder="제목"
             value={newPost.title}
-            onChange={(e) =>
-              onSetNewPost("title", { ...newPost, title: e.target.value })
-            }
+            onChange={(e) => onSetNewPost("title", e.target.value)}
           />
           <Textarea
             rows={30}
             placeholder="내용"
             value={newPost.body}
-            onChange={(e) =>
-              onSetNewPost("body", { ...newPost, body: e.target.value })
-            }
+            onChange={(e) => onSetNewPost("body", e.target.value)}
           />
           <Input
             type="number"
             placeholder="사용자 ID"
             value={newPost.userId}
-            onChange={(e) =>
-              onSetNewPost("userId", {
-                ...newPost,
-                userId: Number(e.target.value),
-              })
-            }
+            onChange={(e) => onSetNewPost("userId", Number(e.target.value))}
           />
           <Button onClick={onAddPost}>게시물 추가</Button>
         </div>
