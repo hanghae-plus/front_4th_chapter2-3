1ec8e1ab2152eb2561a632a863fc54a94e162c19
SU_VIN <pyotnqls25@naver.com>
2025-01-22 22:53:09 +0900

    fix: useSearchStore 상태 독립적으로 관리하도록 변경

--- src/shared/model/useSearchStore.ts
+++ src/shared/model/useSearchStore.ts
@@ -1,45 +1,41 @@
 import { create } from "zustand"
 
-interface SearchParams {
-  search?: string
-  tag?: string
-  sortBy?: string
-  sortOrder?: string
-  skip?: number
-  limit: number
-}
-
 interface SearchStore {
-  searchParams: SearchParams
-  updateSearchParams: (key: keyof SearchParams, value: string | number | undefined) => void
+  search: string
+  tag: string
+  sortBy: string
+  sortOrder: string
+  skip: number
+  limit: number
+  updateSearch: (value: string) => void
+  updateTag: (value: string) => void
+  updateSortBy: (value: string) => void
+  updateSortOrder: (value: string) => void
+  updateSkip: (value: number) => void
+  updateLimit: (value: number) => void
   resetSearchParams: () => void
 }
 
 export const useSearchStore = create<SearchStore>((set) => ({
-  searchParams: {
-    search: "",
-    tag: "",
-    sortBy: "none",
-    sortOrder: "asc",
-    skip: 0,
-    limit: 10,
-  },
-  updateSearchParams: (key, value) =>
-    set((state) => ({
-      searchParams: {
-        ...state.searchParams,
-        [key]: value,
-      },
-    })),
+  search: "",
+  tag: "",
+  sortBy: "none",
+  sortOrder: "asc",
+  skip: 0,
+  limit: 10,
+  updateSearch: (value) => set(() => ({ search: value })),
+  updateTag: (value) => set(() => ({ tag: value })),
+  updateSortBy: (value) => set(() => ({ sortBy: value })),
+  updateSortOrder: (value) => set(() => ({ sortOrder: value })),
+  updateSkip: (value) => set(() => ({ skip: value })),
+  updateLimit: (value) => set(() => ({ limit: value })),
   resetSearchParams: () =>
     set(() => ({
-      searchParams: {
-        search: "",
-        tag: "",
-        sortBy: "none",
-        sortOrder: "asc",
-        skip: 0,
-        limit: 10,
-      },
+      search: "",
+      tag: "",
+      sortBy: "none",
+      sortOrder: "asc",
+      skip: 0,
+      limit: 10,
     })),
 }))
