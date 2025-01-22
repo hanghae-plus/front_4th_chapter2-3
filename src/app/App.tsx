0a9894fd8db84f25a36c03d736549cf10229521b
Geunbaek <rmsqor103@naver.com>
2025-01-23 04:49:11 +0900

    refactor: App 수정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,13 @@ import { Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import PostsManager from "@/pages/PostsManagerPage";
+import { PostsManagerPage } from "@/pages/posts";
 
-import Footer from "@/widgets/ui/Footer";
-import Header from "@/widgets/ui/Header";
+import { Footer, Header } from "@/widgets/layout";
 
 import { Modal } from "@/features/modal";
+
+import { Loading } from "@/shared/ui";
 
 const queryClient = new QueryClient();
 
@@ -19,8 +20,8 @@ const App = () => {
         <div className="flex flex-col min-h-screen">
           <Header />
           <main className="flex-grow container mx-auto px-4 py-8">
-            <Suspense fallback={<>loading...</>}>
-              <PostsManager />
+            <Suspense fallback={<Loading />}>
+              <PostsManagerPage />
             </Suspense>
           </main>
           <Footer />
