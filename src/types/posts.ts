4df84cd778067fcd2f7b74327596e3034bc9065d
chb6734 <chb6734@gmail.com>
2025-01-22 20:40:38 +0900

    ♻️ Post 타입분리

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -1,1 +1,49 @@
- 
+export interface Post {
+  id: number
+  title: string
+  body: string
+  userId: number
+  tags?: string[]
+  reactions?: {
+    likes: number
+    dislikes: number
+  }
+  author?: User
+}
+
+export interface User {
+  id: number
+  username: string
+  image: string
+  firstName?: string
+  lastName?: string
+  age?: number
+  email?: string
+  phone?: string
+  address?: {
+    address: string
+    city: string
+    state: string
+  }
+  company?: {
+    name: string
+    title: string
+  }
+}
+
+export interface Comment {
+  id: number
+  body: string
+  postId: number
+  userId: number
+  likes: number
+  user: {
+    username: string
+  }
+}
+
+export interface Tag {
+  id: number
+  slug: string
+  url: string
+}
