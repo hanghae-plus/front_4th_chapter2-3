dcaaf17067d181b9df55f90d6294422556ba86fa
KimYoungWoo <rladuddn3924@gmail.com>
2025-01-24 01:48:24 +0900

    ✨로깅을 위한 tanstack-query-devtool 의존성 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import PostsManagerPage from '@/pages/PostsManagerPage.tsx';
 import { Header, Footer } from '@/widgets/main';
 import { queryClient } from '@/app/config/query-client.ts';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -15,6 +16,7 @@ const App = () => (
         <Footer />
       </div>
     </Router>
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
 export default App;
