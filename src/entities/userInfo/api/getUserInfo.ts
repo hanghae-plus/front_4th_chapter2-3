ed10641855f2e844b680e5b779ab4a1099085b3d
Dayglow0926 <dayglow0926@gmail.com>
2025-01-24 00:07:35 +0900

    🔥: UserInfo 관련 코드 삭제

--- src/entities/userInfo/api/getUserInfo.ts
+++
@@ -1,6 +0,0 @@
-import { User } from 'src/shared/types';
-
-export const getUserInfo = async (userId: number): Promise<User> => {
-  const response = await fetch(`/api/users/${userId}`);
-  return response.json();
-};
