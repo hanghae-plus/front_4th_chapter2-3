d34b9519985ce8266b69195e1348edb21f442d6c
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2025-01-24 02:38:32 +0900

    :art: Style: Dialog 주석 추가

--- src/widgets/postsManager/ui/PostsManagerWidget.tsx
+++ src/widgets/postsManager/ui/PostsManagerWidget.tsx
@@ -28,6 +28,7 @@ export const PostsManagerWidget = () => {
           </div>
         </CardContent>
       </CardContainer>
+      {/* Dialog */}
       <AddPostDialog />
       <UpdatePostDialog />
       <PostDetailDialog />
