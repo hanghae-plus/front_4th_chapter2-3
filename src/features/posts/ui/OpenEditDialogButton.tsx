ed87e31313900c33dea351928f0fb713b0e23132
Dayglow0926 <dayglow0926@gmail.com>
2025-01-24 03:01:42 +0900

    ✅: Table Row Button 기능 추가

--- src/features/posts/ui/OpenEditDialogButton.tsx
+++ src/features/posts/ui/OpenEditDialogButton.tsx
@@ -1,12 +1,14 @@
 import { Edit2 } from 'lucide-react';
 import { Post } from '../../../shared/types';
 import { Button } from '../../../shared/ui';
+import { usePostStore } from '../model/store';
 
 interface OpenEditDialogButtonProps {
   post: Post;
 }
 
 export const OpenEditDialogButton = ({ post }: OpenEditDialogButtonProps) => {
+  const { setSelectedPost, setShowEditDialog } = usePostStore();
   return (
     <Button
       variant='ghost'
