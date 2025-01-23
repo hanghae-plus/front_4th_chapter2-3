7356a86118de19035d125eca08a60c57d42dcbe7
KimYoungWoo <rladuddn3924@gmail.com>
2025-01-24 01:49:22 +0900

    🎨쿼리키 할당

--- src/entities/tag/api/queries.ts
+++ src/entities/tag/api/queries.ts
@@ -1,8 +1,8 @@
 import { getTags } from '@/entities/tag';
-
+const QUERY_KEY = 'tags';
 export const tagQueries = {
   tags: () => ({
-    queryKey: ['tags'],
+    queryKey: [QUERY_KEY],
     queryFn: getTags,
   }),
 };
