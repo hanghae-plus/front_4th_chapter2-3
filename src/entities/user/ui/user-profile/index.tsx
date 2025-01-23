97a2ab06b84f36fb8ca27e38fbfc641a5a7c5d98
yireum <lastepisode@naver.com>
2025-01-24 00:16:50 +0900

    ♻️ refactor: user 관련 Props Drilling 최소화 수정

--- src/entities/user/ui/user-profile/index.tsx
+++ src/entities/user/ui/user-profile/index.tsx
@@ -1,17 +1,17 @@
-import { User } from "@entities/user/model"
+import { useUserStore } from "@features/user/model/stores"
 
-interface UserProfileProps {
-  user: User | null
+export const UserProfile = () => {
+  const user = useUserStore((state) => state.selectedUser)
+
+  return (
+    <div className="space-y-4">
+      {user && (
+        <>
+          <div>이름: {user.username}</div>
+          <div>이메일: {user.email}</div>
+          <div>전화번호: {user.phone}</div>
+        </>
+      )}
+    </div>
+  )
 }
-
-export const UserProfile = ({ user }: UserProfileProps) => (
-  <div className="space-y-4">
-    {user && (
-      <>
-        <div>이름: {user.username}</div>
-        <div>이메일: {user.email}</div>
-        <div>전화번호: {user.phone}</div>
-      </>
-    )}
-  </div>
-)
