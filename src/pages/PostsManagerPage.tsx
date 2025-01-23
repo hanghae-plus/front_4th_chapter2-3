13fedbabc37e3829f8cf0a50de5c6dc04fedcd52
Blue-Kite <bluelemon112@gmail.com>
2025-01-23 16:49:54 +0900

    feat: highightTest 함수 shared/ui 밑으로 옮기기

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -29,6 +29,7 @@ import UserDialog from "../entities/user/ui/UserDialog"
 import { useGetTags } from "../entities/tag/api/useGetTag"
 import { useUserDialog } from "../entities/user/model/useUserDialog"
 import { useDeletePost } from "../entities/post/api/useDeletePost"
+import { highlightText } from "../shared/lib/highlightText"
 
 interface Post {
   id: number
@@ -460,21 +461,6 @@ const PostsManager = () => {
     setSortOrder(params.get("sortOrder") || "asc")
     setSelectedTag(params.get("tag") || "")
   }, [location.search])
-
-  // 하이라이트 함수 추가
-  const highlightText = (text: string, highlight: string) => {
-    if (!text) return null
-    if (!highlight.trim()) {
-      return <span>{text}</span>
-    }
-    const regex = new RegExp(`(${highlight})`, "gi")
-    const parts = text.split(regex)
-    return (
-      <span>
-        {parts.map((part, i) => (regex.test(part) ? <mark key={i}>{part}</mark> : <span key={i}>{part}</span>))}
-      </span>
-    )
-  }
 
   // 게시물 테이블 렌더링
   const renderPostTable = () => (
