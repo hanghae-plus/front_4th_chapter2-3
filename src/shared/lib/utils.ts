eae01f58b96ee4da089bd4883a284904636301d5
chb6734 <chb6734@gmail.com>
2025-01-24 08:33:45 +0900

    🐛 FSD 폴더 구조 적용 3-6

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+type ClassValue = string | number | boolean | undefined | null | ClassValue[]
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn(...inputs: ClassValue[]): string {
+  return inputs
+    .flat(Infinity)
+    .filter(Boolean)
+    .join(" ")
+    .trim()
+    .split(/\s+/)
+    .filter((value, index, array) => array.indexOf(value) === index)
+    .join(" ")
 }
