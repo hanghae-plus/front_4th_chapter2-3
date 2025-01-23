4f789fc1e7d586147e96771b34479fbafc6944e8
KimYoungWoo <rladuddn3924@gmail.com>
2025-01-23 21:09:56 +0900

    🔇주석 제거

--- src/shared/ui/Table.tsx
+++ src/shared/ui/Table.tsx
@@ -6,7 +6,6 @@ interface TableProps extends React.HTMLAttributes<HTMLTableElement> {
 }
 export const Table = forwardRef<HTMLTableElement, TableProps>(({ className, ...props }, ref) => (
   <div className='w-full overflow-auto'>
-    {/* eslint-disable-next-line sonarjs/table-header */}
     <table
       ref={ref}
       className={`table-fixed w-full caption-bottom text-sm ${className}`}
