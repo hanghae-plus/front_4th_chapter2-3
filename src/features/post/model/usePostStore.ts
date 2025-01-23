fbe57a01a32b0446a4938fc955a7ebcdfa70ecb3
Dana <do.silver2@gmail.com>
2025-01-24 04:14:53 +0900

    feat: useCommentStore 구현, usePostStore 수정

--- src/features/post/model/usePostStore.ts
+++ src/features/post/model/usePostStore.ts
@@ -6,12 +6,18 @@ interface PostState {
   total: number;
   selectedPost: PostWithUser | null;
   newPost: NewPost;
+  showAddDialog: boolean;
+  showEditDialog: boolean;
+  showPostDetailDialog: boolean;
   setPosts: (posts: PostWithUser[]) => void;
   addPost: (post: PostWithUser) => void;
   updatePost: (updatedPost: PostWithUser) => void;
   deletePost: (postId: number) => void;
   setSelectedPost: (selectedPost: PostWithUser) => void;
   setNewPost: (newPost: NewPost) => void;
+  setShowAddDialog: (value: boolean) => void;
+  setShowEditDialog: (value: boolean) => void;
+  setShowPostDetailDialog: (value: boolean) => void;
 }
 
 export const usePostStore = create<PostState>((set) => ({
@@ -19,6 +25,9 @@ export const usePostStore = create<PostState>((set) => ({
   total: 0,
   selectedPost: null,
   newPost: { title: "", body: "", userId: 1 },
+  showAddDialog: false,
+  showEditDialog: false,
+  showPostDetailDialog: false,
   setPosts: (posts) => set({ posts, total: posts.length }),
   addPost: (post) => set((state) => ({ posts: [...state.posts, post] })),
   updatePost: (updatedPost) =>
@@ -26,4 +35,7 @@ export const usePostStore = create<PostState>((set) => ({
   deletePost: (id) => set((state) => ({ posts: state.posts.filter((post) => post.id !== id) })),
   setSelectedPost: (selectedPost) => set({ selectedPost }),
   setNewPost: (newPost) => ({ newPost }),
+  setShowAddDialog: (showAddDialog) => set({ showAddDialog }),
+  setShowEditDialog: (showEditDialog) => set({ showEditDialog }),
+  setShowPostDetailDialog: (showPostDetailDialog) => set({ showPostDetailDialog }),
 }));
