2c56be2d0d9cfa19cdd94c675c6657086f66f1d3
9yurilee <9yurilee@gmail.com>
2025-01-23 17:42:13 +0900

    refactor: Post 관련 API URL 상수로 관리

--- src/entities/post/api/postApi.ts
+++ src/entities/post/api/postApi.ts
@@ -1,18 +1,21 @@
 import { api } from "../../../shared/api/api";
 import { Post } from "../model/types";
 
+// URL 상수로 관리
+const API_POST = "/api/posts";
+
 export const getPosts = async (limit: number, skip: number) => {
-  const response = await api.get(`/api/posts?limit=${limit}&skip=${skip}`);
+  const response = await api.get(`${API_POST}?limit=${limit}&skip=${skip}`);
   return response.data;
 };
 
 export const getPostsByTag = async (tag: string) => {
-  const response = await api.get(`/api/posts/tag/${tag}`);
+  const response = await api.get(`${API_POST}/tag/${tag}`);
   return response.data;
 };
 
 export const searchPost = async (searchQuery: string) => {
-  const response = await api.get(`/api/posts/search?q=${searchQuery}`);
+  const response = await api.get(`${API_POST}/search?q=${searchQuery}`);
   return response.data;
 };
 
@@ -21,16 +24,16 @@ export const addNewPost = async (newPost: {
   body: string;
   userId: number;
 }) => {
-  const response = await api.post("/api/posts/add", newPost);
+  const response = await api.post(`${API_POST}/add`, newPost);
   return response.data;
 };
 
 export const updatePost = async (postId: number, selectedPost: Post) => {
-  const response = await api.put(`/api/posts/${postId}`, selectedPost);
+  const response = await api.put(`${API_POST}/${postId}`, selectedPost);
   return response.data;
 };
 
 export const deletePost = async (id: number) => {
-  const response = await api.delete(`/api/posts/${id}`);
+  const response = await api.delete(`${API_POST}/${id}`);
   return response.data;
 };
