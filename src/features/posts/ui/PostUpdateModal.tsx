b61c58bb3049fff06428aaae4ed3a7a28fe06afe
Geunbaek <rmsqor103@naver.com>
2025-01-23 23:55:56 +0900

    fix: 게시물 수정 안되는 버그 수정

--- src/features/posts/ui/PostUpdateModal.tsx
+++ src/features/posts/ui/PostUpdateModal.tsx
@@ -30,14 +30,14 @@ export const PostUpdateModal = ({ post }: PostUpdateModal) => {
       <div className="space-y-4">
         <Input
           placeholder="제목"
-          value={post.title}
-          onChange={(e) => setEditedPost({ ...post, title: e.target.value })}
+          value={editedPost.title}
+          onChange={(e) => setEditedPost({ ...editedPost, title: e.target.value })}
         />
         <Textarea
           rows={15}
           placeholder="내용"
-          value={post.body}
-          onChange={(e) => setEditedPost({ ...post, body: e.target.value })}
+          value={editedPost.body}
+          onChange={(e) => setEditedPost({ ...editedPost, body: e.target.value })}
         />
         <Button onClick={handleUpdatePost}>게시물 업데이트</Button>
       </div>
