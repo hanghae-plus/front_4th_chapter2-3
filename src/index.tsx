293c0d61eb161329ba05b1cae72980b8171cd044
JaeHyun Bang <jhbang@plea.kr>
2025-01-24 10:28:00 +0900

    feat : Jotai provider 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter as Router } from "react-router-dom"
 import App from "./App"
+import { StoreProvider } from "./app/providers/StoreProvider"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </Router>
   </React.StrictMode>,
 )
