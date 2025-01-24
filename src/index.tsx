bed8d29353d7f8104bb9134c6fedd63e37b96562
9yurilee <9yurilee@gmail.com>
2025-01-24 14:27:47 +0900

    chore: QueryClientProvider 관련 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <App />
-      </Router>
-    </QueryClientProvider>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
